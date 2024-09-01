40f21b750948df424a2cbabe8cc4ce0146a15aca
harshyadav02 <harshyadav5736@gmail.com>
2024-09-01 16:30:57 +0530

    testing related files

---
+++ src/test/java/com/harsh/JWTLearnings/JwtBasedSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.JWTLearnings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBasedSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
