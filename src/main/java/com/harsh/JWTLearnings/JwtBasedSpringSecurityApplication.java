9824b4f8c1fd8e486f6f0161cde8dc8732cdee29
harshyadav02 <harshyadav5736@gmail.com>
2024-09-01 16:25:32 +0530

    Entry file for the project

---
+++ src/main/java/com/harsh/JWTLearnings/JwtBasedSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.JWTLearnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBasedSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtBasedSpringSecurityApplication.class, args);
+	}
+
+}
