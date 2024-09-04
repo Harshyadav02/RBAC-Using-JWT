fa975dba66f53c01bf1458f125705908fe516110
harshyadav02 <harshyadav5736@gmail.com>
2024-09-04 10:28:58 +0530

    admin related inforamtion

---
+++ src/main/java/com/harsh/JWTLearnings/Entity/Admin.java
@@ -0,0 +1,15 @@
+package com.harsh.JWTLearnings.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Admin {
+    @Id
+    private String email;
+    @Column(nullable = false)
+    private String password;
+}
