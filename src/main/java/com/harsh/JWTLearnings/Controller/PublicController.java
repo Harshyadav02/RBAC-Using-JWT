70984f7cc2b63827216906cfde9135be8803c147
harshyadav02 <harshyadav5736@gmail.com>
2024-09-04 10:58:20 +0530

    contains route for signin

---
+++ src/main/java/com/harsh/JWTLearnings/Controller/PublicController.java
@@ -0,0 +1,29 @@
+package com.harsh.JWTLearnings.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.harsh.JWTLearnings.Entity.Admin;
+import com.harsh.JWTLearnings.Service.AdminService;
+
+@RestController
+
+public class PublicController {
+    @Autowired
+    private AdminService adminService;
+
+    // method to create new admin
+    @PostMapping("/signin")
+    public ResponseEntity<?> createAdmin(@RequestBody Admin adminDetails){
+
+        try {
+            return adminService.createAdmin(adminDetails);
+        } catch (Exception e) {
+           return new ResponseEntity<>(e.getLocalizedMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
