77bbf3d5320b15fd983df5ab388c083ab1ac4394
harshyadav02 <harshyadav5736@gmail.com>
2024-09-05 07:08:29 +0530

    Repository annotaion added

--- src/main/java/com/harsh/JWTLearnings/Repository/AdminRepo.java
+++ src/main/java/com/harsh/JWTLearnings/Repository/AdminRepo.java
@@ -1,9 +1,11 @@
 package com.harsh.JWTLearnings.Repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.harsh.JWTLearnings.Entity.Admin;
 
+@Repository
 public interface AdminRepo extends CrudRepository<Admin,String>{
 
     public Admin findByEmail(String email);
