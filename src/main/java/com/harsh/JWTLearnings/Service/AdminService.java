e9b7f2d4a650b351a1c6838d3f98478d57b17557
harshyadav02 <harshyadav5736@gmail.com>
2024-09-05 07:17:13 +0530

    Return type changed to ResponseEntity of createAdmin

--- src/main/java/com/harsh/JWTLearnings/Service/AdminService.java
+++ src/main/java/com/harsh/JWTLearnings/Service/AdminService.java
@@ -4,25 +4,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.harsh.JWTLearnings.Entity.Admin;
 import com.harsh.JWTLearnings.Repository.AdminRepo;
+import com.harsh.JWTLearnings.Security.JWTUtils;
+import com.harsh.JWTLearnings.Security.UserDetailsServiceImp;
 
+@Service
 public class AdminService {
     /* Injecting admin repo and password encoder */
     @Autowired
     private AdminRepo adminRepo;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserDetailsServiceImp userDetailsServiceImp;
 
-    
+    @Autowired
+    private JWTUtils jwtUtils;
+
     public ResponseEntity<?> createAdmin(Admin adminDetails) {
-        
+
         // encrytping password
         adminDetails.setPassword(passwordEncoder.encode(adminDetails.getPassword()));
         adminRepo.save(adminDetails);
 
-        return new ResponseEntity<>("Admin created sucessfully",HttpStatus.OK);
+        return new ResponseEntity<>("Admin created sucessfully", HttpStatus.OK);
     }
-    
+
+    public ResponseEntity<String> loginUser(Admin adminDetails) {
+
+        return new ResponseEntity<>(jwtUtils
+        .generateTokenFromUsername(
+            userDetailsServiceImp
+                .loadUserByUsername(adminDetails
+                        .getEmail())),HttpStatus.OK);
+    }
+
 }
