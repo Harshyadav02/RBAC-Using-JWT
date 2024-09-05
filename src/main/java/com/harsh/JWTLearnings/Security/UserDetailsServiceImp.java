e1563985646fd51d175de171cac28f159598007e
harshyadav02 <harshyadav5736@gmail.com>
2024-09-05 07:16:12 +0530

    Logic to load Admin by email

--- src/main/java/com/harsh/JWTLearnings/Security/UserDetailsServiceImp.java
+++ src/main/java/com/harsh/JWTLearnings/Security/UserDetailsServiceImp.java
@@ -7,7 +7,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.harsh.JWTLearnings.Entity.Admin;
 import com.harsh.JWTLearnings.Entity.Employee;
+import com.harsh.JWTLearnings.Repository.AdminRepo;
 import com.harsh.JWTLearnings.Repository.EmployeeRepo;
 
 /**
@@ -18,9 +20,21 @@ public class UserDetailsServiceImp implements UserDetailsService {
 
     @Autowired
     private EmployeeRepo employeeRepo;
-
+    @Autowired
+    private AdminRepo adminRepo;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+
+        Admin adminDetail = adminRepo.findByEmail(email);
+        if (adminDetail != null) {
+            // Return UserDetails object with admin details
+            return User.builder()
+                       .username(adminDetail.getEmail())
+                       .password(adminDetail.getPassword())
+                       .roles(adminDetail.getRole())  // Roles/authorities
+                       .build();
+        }
         // Find employee by email
         Employee employeeDetail = employeeRepo.findByEmail(email);
         if (employeeDetail != null) {
@@ -31,6 +45,7 @@ public class UserDetailsServiceImp implements UserDetailsService {
                        .roles(employeeDetail.getRole())  // Roles/authorities
                        .build();
         }
+
         // Throw exception if user not found
         throw new UsernameNotFoundException("No user found with email: " + email);
     }
