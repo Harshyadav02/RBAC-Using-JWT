fe85096fafd6f9320f223d75b28653c3dbe8efef
harshyadav02 <harshyadav5736@gmail.com>
2024-09-05 08:51:41 +0530

    Route protected based on admin role

--- src/main/java/com/harsh/JWTLearnings/Controller/AdminController.java
+++ src/main/java/com/harsh/JWTLearnings/Controller/AdminController.java
@@ -2,15 +2,17 @@ package com.harsh.JWTLearnings.Controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/admin/")
 public class AdminController {
 
     @GetMapping("/hello/")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> hello(){
         try {
             return new ResponseEntity<>("Hello admin",HttpStatus.OK);
