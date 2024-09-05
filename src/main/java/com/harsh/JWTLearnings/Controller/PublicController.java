ee9af194c5c3e29362e2805bf51bfd2e1a80f116
harshyadav02 <harshyadav5736@gmail.com>
2024-09-05 08:52:33 +0530

    prefix added to each route based on admin and employee

--- src/main/java/com/harsh/JWTLearnings/Controller/PublicController.java
+++ src/main/java/com/harsh/JWTLearnings/Controller/PublicController.java
@@ -11,16 +11,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.harsh.JWTLearnings.Entity.Admin;
+import com.harsh.JWTLearnings.Entity.Employee;
 import com.harsh.JWTLearnings.Service.AdminService;
+import com.harsh.JWTLearnings.Service.EmployeeService;
 
 @RestController
 public class PublicController {
     @Autowired
     private AdminService adminService;
 
+    @Autowired
+    private EmployeeService empService;
+
     // method to create new admin
-    @PostMapping("/signin/")
-    public ResponseEntity<?> signin(@RequestBody Admin adminDetails) {
+    @PostMapping("/admin/signin/")
+    public ResponseEntity<?> createAdmin(@RequestBody Admin adminDetails) {
 
         try {
             return adminService.createAdmin(adminDetails);
@@ -29,15 +34,14 @@ public class PublicController {
         }
     }
 
-    @PostMapping("/login/")
-    public ResponseEntity<?> login(@RequestBody Admin adminDetails) {
+    @PostMapping("/admin/login/")
+    public ResponseEntity<?> adminLogin(@RequestBody Admin adminDetails) {
         try {
             String token = adminService.loginUser(adminDetails);
 
             Map<String, String> response = new HashMap<>();
             response.put("token", token);
 
-            
             return new ResponseEntity<>(response, HttpStatus.OK);
 
         } catch (Exception e) {
@@ -45,4 +49,27 @@ public class PublicController {
         }
     }
 
+    @PostMapping("/emp/login/")
+    public ResponseEntity<?> empLogin(@RequestBody Employee empDetails) {
+        try {
+            String token = empService.loginUser(empDetails);
+
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PostMapping("/emp/signin/")
+    public ResponseEntity<?> createEmployee(@RequestBody Employee empDetails) {
+
+        try {
+            return empService.createEmployee(empDetails);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
