938eb6cf9fe3975fa77d35d8dd8ff14dfb033deb
harshyadav02 <harshyadav5736@gmail.com>
2024-09-05 07:08:53 +0530

    Repository annotaion added

--- src/main/java/com/harsh/JWTLearnings/Repository/EmployeeRepo.java
+++ src/main/java/com/harsh/JWTLearnings/Repository/EmployeeRepo.java
@@ -1,9 +1,10 @@
 package com.harsh.JWTLearnings.Repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.harsh.JWTLearnings.Entity.Employee;
-
+@Repository
 public interface EmployeeRepo extends CrudRepository<Employee, String>{
     public Employee findByEmail(String email);
 }
