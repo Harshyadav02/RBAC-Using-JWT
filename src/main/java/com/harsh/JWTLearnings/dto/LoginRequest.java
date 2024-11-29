aae64bb67bf60357d6f8483c9760c603782338f4
harshyadav02 <harshyadav5736@gmail.com>
2024-11-29 17:08:50 +0530

    DTO for login request

--- src/main/java/com/harsh/JWTLearnings/dto/LoginRequest.java
+++ src/main/java/com/harsh/JWTLearnings/dto/LoginRequest.java
@@ -1,4 +1,14 @@
 package com.harsh.JWTLearnings.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+/* This class is used for login request  for the transfer of data*/
+@Data
 public class LoginRequest {
+
+    @NotNull(message = "please enter email")
+    private  String email;
+    @NotNull(message = "please enter password")
+    private String password;
 }
