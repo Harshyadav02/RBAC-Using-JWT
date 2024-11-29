2064734f48476bcc29c348667af8f63252f4aaf2
harshyadav02 <harshyadav5736@gmail.com>
2024-11-29 17:09:24 +0530

    Gloabal Exception class that handle customized exception

---
+++ src/main/java/com/harsh/JWTLearnings/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.harsh.JWTLearnings.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/* This class  handle user defined exception */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<String> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
+        String message = String.format(
+            "Method %s is not supported for this request. Supported methods are: %s",
+            ex.getMethod(),
+            ex.getSupportedHttpMethods()
+        );
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(message);
+    }
+
+    
+}
