15f976e9ef7306e70402b5748248e23bb21c6164
harshyadav02 <harshyadav5736@gmail.com>
2024-11-29 19:09:30 +0530

    Password checker added

--- src/main/java/com/harsh/JWTLearnings/Service/PublicService.java
+++ src/main/java/com/harsh/JWTLearnings/Service/PublicService.java
@@ -3,6 +3,7 @@ package com.harsh.JWTLearnings.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,31 +20,39 @@ public class PublicService {
     @Autowired
     private PasswordEncoder passwordEncoder;
     @Autowired
-    private  UserRepo userRepo;
+    private UserRepo userRepo;
     @Autowired
     private UserDetailsServiceImp userDetailsServiceImp;
     @Autowired
     private JWTUtils jwtUtils;
 
     // login method that genrate a JWT token after user credentails validation
+    
     public String loginUser(LoginRequest loginRequest) {
         try {
 
-            return jwtUtils
-                    .generateTokenFromUsername(userDetailsServiceImp
-                            .loadUserByUsername(loginRequest.getEmail()));
-        } catch (Exception e) {
+            // Load user details using the email 
+            UserDetails userDetails = userDetailsServiceImp.loadUserByUsername(loginRequest.getEmail());
 
+            // Check if the entered password matches the stored password
+            if (passwordEncoder.matches(loginRequest.getPassword(), userDetails.getPassword())) {
+               
+                return jwtUtils.generateTokenFromUsername(userDetails);
+            } else {
+                
+                return "Invalid password!";
+            }
+        } catch (Exception e) {
+           
             return e.getLocalizedMessage();
         }
-
     }
 
     // Method that create a user and save to db
     public ResponseEntity<?> createUser(UserEntity userDetails) {
         try {
             String email = userDetails.getEmail();
-            if(userRepo.findByEmail(email)!=null){
+            if (userRepo.findByEmail(email) != null) {
                 throw new Exception("Email already exist");
             }
             userDetails.setPassword(passwordEncoder.encode(userDetails.getPassword()));
@@ -55,5 +64,4 @@ public class PublicService {
 
     }
 
-    
 }
